bc3efdf1814218921d66b99c4844ff09e4e3368e
152772a <152772a@mymail.nyp.edu.sg>
2016-11-14 15:10:08 +0800

    i

--- src/demo/Hi.java
+++ src/demo/Hi.java
@@ -1,15 +1,24 @@
 package demo;
-
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
 
 @Path("/hi")
 public class Hi {
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.TEXT_XML)
     public String getMessage() {
-        return "Hi!";
+        return "<hi>Hi World!</hi>";
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getPlainMessage() {
+        return "Hi World!";
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public String getHTMLMessage() {
+        return "<b>Hi World!</b>";
     }
 }
