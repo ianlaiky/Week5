95b9cd543ad4e54b9c52eba120e430ea599ef755
152772a <152772a@mymail.nyp.edu.sg>
2016-11-14 15:19:20 +0800

    i

---
+++ src/demo/Book.java
@@ -0,0 +1,17 @@
+package demo;
+
+
+public class Book {
+    String title;
+    String description;
+    String author;
+    int year;
+
+
+    public Book() {
+        this.title = "Java Made Easy";
+        this.description = "A book about how to learn Java in a painless manner";
+        this.author = "Java Duke";
+        this.year = 2016;
+    }
+}
